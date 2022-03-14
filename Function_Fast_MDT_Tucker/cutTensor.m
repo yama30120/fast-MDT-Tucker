9af40f3ad5b45ea20f7fd5abf73c2367c0771c41
mtbkry <mtb0x0010@gmail.com>
2022-03-14 10:57:18 +0900

    add comment in cutTensor

--- Function_Fast_MDT_Tucker/cutTensor.m
+++ Function_Fast_MDT_Tucker/cutTensor.m
@@ -1,4 +1,21 @@
 function [Y, dimY] = cutTensor(X, dimX, mode, idx)
+% input:
+%  X: input tensor
+%  dimX: dimension of X
+%  mode: mode to index
+%  idx: index in mode
+% output:
+%  Y: cut tensor by idx in the mode
+%  dimY: dimension of Y
+% e.g.
+%  3rd order tensor (2 x 2 x 3)
+%  X = reshape(1:12, [2 2 3])
+%    = [1 3;  [5 7;  [ 9 11;
+%       2 4],  6 8], [10 12]
+%  cutTensor(X, [2 2 3], 3, [1 3]):
+%  Y = [1 3;  [ 9 11;
+%       2 4], [10 12]
+%  dimY = [2 2 2]
     N = length(dimX);
     if N == 1
         Y = X(idx);
